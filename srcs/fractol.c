fa16b4a6aba5b22552d0f5194a5a7f680e2061ce
maoow <corentin34530@hotmail.fr>
2017-11-21 21:42:38 +0100

    hook

--- srcs/fractol.c
+++ srcs/fractol.c
@@ -16,17 +16,18 @@ void	mand(t_fractenv *env, t_pixel pixel)
 	t_pixel	z;
 	t_pixel	p;
 
-	p.x = (((pixel.x - 2 + env->x)/ env->zoom )) ;// (env->size / 4) ;
-	p.y =  (((pixel.y - 2 + env->y) / env->zoom)); // (env->size / 8) ;
+	p.x = (((pixel.x - 2 + env->x )/ env->zoom )) ;// (env->size / 4) ;
+	p.y =  (((pixel.y - 2  + env->y) / env->zoom)); // (env->size / 8) ;
 	z.x = 0;
 	z.y = 0;
 	i = 0;
-	while (i < env->it_max && z.x * z.x + z.y * z.y < 2)
+	while (i < env->it_max && z.x * z.x + z.y * z.y < env->max && (z.x * z.x + z.y * z.y > env->min || i == 0))
 	{
 		csquare(&z);
 		z.x += p.x;
 		z.y += p.y;
 		i++;	
+		mlx_pixel_put(env->mlx, env->win, pixel.x , pixel.y , i * 256);
 	}
 	if (i == env->it_max)
 		mlx_pixel_put(env->mlx, env->win, pixel.x , pixel.y ,0x00ff00);
@@ -58,11 +59,14 @@ int	main()
 env.zoom = 100;
 env.x = -600;
 env.y = -300;
+env.min = -1000;
+env.max = 9;
 	env.win = mlx_new_window(env.mlx, 1200,600, "fract");
 	env.size = 1200;
 	env.it_max = 10;
 	fract(&env, &mand);
 	mlx_key_hook(env.win, &keypressed, &env);
+	mlx_hook(env.win, MN, MM, &mousemove, &env);
 	mlx_mouse_hook(env.win, &buttonpressed, &env);
 	mlx_loop(env.mlx);
 }
