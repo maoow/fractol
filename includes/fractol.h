e22a9bc4a059dc1d281fea36d3d07ebab4eba628
maoow <corentin34530@hotmail.fr>
2017-11-21 21:43:30 +0100

    min/max and mouse move

--- includes/fractol.h
+++ includes/fractol.h
@@ -1,9 +1,13 @@
 #include "mlx.h"
+//#include <X11.h>
+#include "mlx_int.h"
 #include "ft_printf.h"
 #include "libft.h"
 #include "get_next_line.h"
 #define K_NB 9
 #define B_NB 5
+#define MN 6
+#define MM (1L<<6)
 
 typedef struct	s_pixel
 {
@@ -14,6 +18,8 @@ typedef struct	s_pixel
 
 typedef struct	s_fractenv
 {
+	double	min;
+	double	max;
 	double	size;
 	double	zoom;
 	double	x;
@@ -27,6 +33,7 @@ int key;
 }		t_fractenv;
 int			keypressed(int key, t_fractenv *env);
 int			buttonpressed(int key,int x,int y, t_fractenv *env);
+int			mousemove(int x,int y, t_fractenv *env);
 
 
 void			mouserotate(t_fractenv *env);
@@ -41,4 +48,4 @@ void			zoom(t_fractenv *env);
 void			dezoom(t_fractenv *env);
 void			harder(t_fractenv *env);
 void			smoother(t_fractenv *env);
-void	mand(t_fractenv *env, t_pixel pixel);
+void			mand(t_fractenv *env, t_pixel pixel);
