0f0eb05505d99f34e93947f258988c1a617c56e6
maoow <corentin34530@hotmail.fr>
2017-11-25 09:25:29 +0100

    zoom following mouse

--- srcs/move.c
+++ srcs/move.c
@@ -7,36 +7,52 @@ void			center(t_fractenv *env)
 }
 void			left(t_fractenv *env)
 {
-env->x-= 100;
+env->x-= env->width / 4;
 }
 void			up(t_fractenv *env)
 {
-env->y-= 100;
+env->y-= env->height / 4;
 }
 void			down(t_fractenv *env)
 {
-env->y+= 100;
+env->y+= env->height / 4;
 }
 void			right(t_fractenv *env)
 {
-env->x+= 100;
+env->x+= env->width / 4;
 }
 void			higher(t_fractenv *env)
 {
-env->it_max *=2;
+env->it_max += 10;
 }
 void			lower(t_fractenv *env)
 {
-env->it_max /=2;
+env->it_max -= 10;
 }
 void			zoom(t_fractenv *env)
 {
-env->zoom *= 2;
+	int speed = 1;
+if (env->mouse.x)
+{
+env->x += (env->x + env->width / 2)/ 10;
+env->y += (env->y + env->height / 2) / 10;
+env->zoom += (env->zoom / 10) * speed;
+env->x += ((env->mouse.x - env->width / 2) / 10);// * sqrt(env->zoom);
+env->y += ((env->mouse.y - env->height / 2) /  10); //* sqrt(env->zoom);
+	fract(env, &mand);
+}
+//env->zoom *= 2;
+//env->x += (env->x + env->width / 2);
+//env->y += (env->y + env->height / 2);
 }
 void			dezoom(t_fractenv *env)
 {
 if (env->zoom >= 2)
+{
+env->x -= (env->x + env->width / 2);
+env->y -= (env->y + env->height / 2);
 env->zoom /= 2;
+}
 }
 void			harder(t_fractenv *env)
 {
